e44d1534b88181ac35e27f07b2aa65c372949f77
saiqamardi16 <saiqamardia@gmail.com>
2026-03-26 17:24:22 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 #This is the main file that runs the game. 
 # It imports the pygame class from the game module and creates an instance of it,
 #  then calls the run method to show the game with the most recent updates.
+
 from game import pygame
 
 if __name__ == "__main__":
