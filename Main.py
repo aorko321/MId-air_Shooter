f10da79392fbc1581caff6b2f1bd4bf8154c725c
aorko321 <aorko12009@gmail.com>
2026-03-28 23:37:44 +0600

    eletre

--- Main.py
+++ Main.py
@@ -1,10 +1,4 @@
-# HEAD  
-#This is the main file that runs the game. 
-# It imports the pygame class from the game module and creates an instance of it,
-#  then calls the run method to show the game with the most recent updates.
-
 from game import ShooterGame
-
 
 if __name__ == "__main__":
     game = ShooterGame()
